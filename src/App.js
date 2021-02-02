6925d990931846da178243cf0916d7eb550f1868
Matthew Sevey <mjsevey@gmail.com>
2021-02-02 13:42:26 -0700

    Update styling and create basic form

--- src/App.js
+++ src/App.js
@@ -3,7 +3,26 @@ import './App.css';
 function App() {
 	return (
 		<div className="App">
-			<header className="App-header"></header>
+			<div>
+				<h2>Welcome to Skynet!</h2>
+			</div>
+			{/* Basic input form */}
+			<div className="basic-form">
+				{/* Input for name */}
+				<label className="form-label">Name</label>
+				<input type="text" />
+
+				<br />
+				<br />
+
+				{/* Input for file */}
+				<label className="form-label">File</label>
+				<input type="file" />
+
+				<br />
+				<br />
+				<button className="form-button">Send to Skynet</button>
+			</div>
 		</div>
 	);
 }
